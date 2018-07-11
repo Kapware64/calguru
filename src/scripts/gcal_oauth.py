8fdbd40b4b75b016e9e6d1d84db86d9d362dbacb
Noah Kaplan <noahkaplan@MacBook-Pro-130.local>
2018-07-11 13:42:13 -0400

    Add basic Google Calender api access; add readme

--- src/scripts/gcal_oauth.py
+++ src/scripts/gcal_oauth.py
@@ -4,17 +4,16 @@ gcal_credentials.json. Account used to sign in with here is used for all Google
 Calendar API calls.
 """
 
-from __future__ import print_function
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
+from src.api.gcal_api import GoogleCalendarApi
 
 # Facilitate Google OAuth sign-in via browser
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-store = file.Storage('../conf/gcal_credentials.json')
+store = file.Storage(GoogleCalendarApi.CREDENTIALS_DIR)
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets(
-        '../conf/gcal_client_secret.json', SCOPES)
+        GoogleCalendarApi.CLIENT_SECRET_DIR, GoogleCalendarApi.SCOPE)
     creds = tools.run_flow(flow, store)
 service = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
