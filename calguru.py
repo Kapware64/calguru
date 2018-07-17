fc89511168fe7036e45549729ac138c36fba9b0f
Noah Kaplan <noahkaplan@MacBook-Pro-130.local>
2018-07-17 12:46:04 -0400

    Add bottle endpoint and basics of endpoint testing

--- calguru.py
+++ calguru.py
@@ -1,5 +1,6 @@
-from bottle import route, run
+from bottle import Bottle, route, run, request
 from src.api.gcal_api import GoogleCalendarApi
+from src.utils.api_utils import APIUtils
 
 
 @route('/')
@@ -7,6 +8,31 @@ def index():
     """Home page."""
 
     return "CalGuru"
+
+
+@APIUtils.api_decorator
+def create_gcal_events():
+    """
+    Called when endpoint for creating Google Calendar events is invoked.
+
+    Content-Type: application/json
+    Input body: Json with 'events' field containing Json array. Each
+    item in array specifies an event and should follow these keys:
+       'summary': Event summary. Required.
+       'start': UTC timestamp of event starting time. Required.
+       'end': UTC timestamp of event ending time. Required.
+       'attendees' = List of attendee emails.
+       'description' = Event description.
+       'location' = Event location.
+    Output: Created events' ids, summaries, and links in json.
+    """
+
+    # Create events and store Google Calendar info of created events
+    gcal_events_info = GoogleCalendarApi.batch_create_events(
+        APIUtils.get_body(request)['events'])
+
+    # Return created events' ids, summaries, and links
+    return {'calendar_events': gcal_events_info}
 
 
 @route('/upcoming')
@@ -20,6 +46,9 @@ def upcoming():
 
     return GoogleCalendarApi.get_next_event()
 
+
+# Route for creating Google Calendar events
+Bottle().post("/gcal/events", callback=create_gcal_events)
 
 # Should match valid redirect uris in OAuth client secret files
 run(host='localhost', port=8080, debug=True)
---
+++ test/test_calguru.py
@@ -0,0 +1,51 @@
+import unittest
+import arrow
+import calguru
+from webtest import TestApp
+
+
+class CalGuruTest(unittest.TestCase):
+    """Test user_res.py"""
+
+    # Milliseconds in an hour
+    HOURS_MILLIS = 3600
+
+    def setUp(self):
+        """Setup method for testing"""
+
+        self.app = TestApp(calguru)
+
+    def test_create_gcal_events(self):
+        """Test we can add event via POST /gcal_events REST API endpoint"""
+
+        # UTC timestamp representing 2pm, January 2nd, 2017
+        utc_timestamp_jan_2 = arrow.get(2017, 1, 2, 14).timestamp
+
+        # Event to create
+        event = {
+            'summary': "Test",
+            'start': utc_timestamp_jan_2,
+            'end': utc_timestamp_jan_2 + 5 * CalGuruTest.HOURS_MILLIS,
+            'attendees': ["test.thecalguru@gmail.com"],
+            'description': 'A test event',
+            'location': 'MongoDB'
+        }
+
+        # Event to create
+        event_missing_fields = {
+            'summary': "Test"
+        }
+
+        # Make valid post-add request
+        valid_resp = self.app.post_json(
+            '/gcal/events', {'events': [event]})
+
+        # Assert successful status code
+        self.assertEqual(valid_resp.status_code, 200)
+
+        # Make invalid post-add request
+        invalid_resp = self.app.post_json(
+            '/gcal/events', {'events': [event_missing_fields]})
+
+        # Assert failed status code
+        self.assertEqual(invalid_resp.status_code, 400)
